a6ebf7fffb526ac84be216d5678672ebe8b3216d
sheafitzek <agg817@sbcglobal.net>
2018-02-15 22:28:39 -0600

    add logo & search icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import styled from 'styled-components';
 
+import logo from '../assets/images/logo.png';
+
 export class SearchBar extends Component {
 	constructor(props) {
 		super(props);
@@ -11,10 +13,13 @@ export class SearchBar extends Component {
 	render() {
 		return (
 			<Wrapper className="search-bar">
+				<i className="fa fa-search" />
 				<input
+					placeholder="Search YouTube"
 					value={this.state.term}
 					onChange={(e) => this.setState({term: e.target.value})}
 				/>
+				<img src={logo} alt="logo" />
 			</Wrapper>
 		);
 	}
@@ -26,7 +31,26 @@ const Wrapper = styled.div`
 	margin: 1.25rem;
 	text-align: center;
 
+	i {
+		height: 2.5rem;
+		color: #df4309;
+		margin-right: 0.5rem;
+	}
+
 	input {
 		width: 75%;
+
+		&:before {
+			content: "\f072";
+			font-family: "FontAwesome";
+			font-size: 10rem;
+			padding: 6px;
+		}
+	}
+
+	img {
+		height: 1.5rem;
+		margin-left: 0.5rem;
+		margin-bottom: 0.25rem;
 	}
 `;
