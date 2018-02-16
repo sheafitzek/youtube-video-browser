4bdf69b8685793d81e394a2f6a1e01543977f506
sheafitzek <agg817@sbcglobal.net>
2018-02-15 22:23:36 -0600

    debounce api call & store last term in local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import _ from 'lodash';
+
 import SearchBar from './SearchBar.js';
 import VideoList from './VideoList.js';
 import VideoDetail from './VideoDetail.js';
@@ -17,7 +19,13 @@ export class App extends Component {
 			selectedVideo : null,
 		};
 
-		ytSearch({key: API_KEY, term: `surfboards`}, (videos) => {
+		const storedTerm = localStorage.getItem(`youtube-video-browser-term`);
+
+		this.videoSearch(storedTerm ? storedTerm : `texas`);
+	}
+
+	videoSearch(term) {
+		ytSearch({key: API_KEY, term}, (videos) => {
 			this.setState({
 				videos,
 				selectedVideo : videos[0],
@@ -26,9 +34,14 @@ export class App extends Component {
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => {
+			this.videoSearch(term);
+			localStorage.setItem(`youtube-video-browser-term`, term);
+		}, 300);
+
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList
 					onVideoSelect={(selectedVideo) =>
